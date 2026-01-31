af944201833dc2b1f139f8f3629619a4c5e313d6
RivasMilton <00019725@uca.edu.sv>
2026-01-31 15:16:28 -0600

    add: clase insertarInicio

--- main.cpp
+++ main.cpp
@@ -9,3 +9,24 @@ struct Nodo {
     Nodo* ant;
 };
  
+Nodo* head = NULL;
+Nodo* tail = NULL;
+int contador = 0;
+void insertarInicio(int id, string nombre, float peso) {
+    Nodo* nuevo = new Nodo();
+    nuevo->id = id;
+    nuevo->nombre = nombre;
+    nuevo->peso = peso;
+
+    if (head == NULL) {
+        head = tail = nuevo;
+        nuevo->sig = NULL;
+        nuevo->ant = NULL;
+    } else {
+        nuevo->sig = head;
+        nuevo->ant = NULL;
+        head->ant = nuevo;
+        head = nuevo;
+    }
+    contador++;
+}
