d5c992f8ce6b3a9dae50a559c24009624785fa33
RivasMilton <00019725@uca.edu.sv>
2026-01-31 15:18:48 -0600

    add: creacion de las demas clases

--- main.cpp
+++ main.cpp
@@ -30,3 +30,94 @@ void insertarInicio(int id, string nombre, float peso) {
     }
     contador++;
 }
+
+void insertarFinal(int id, string nombre, float peso) {
+    Nodo* nuevo = new Nodo();
+    nuevo->id = id;
+    nuevo->nombre = nombre;
+    nuevo->peso = peso;
+
+    if (head == NULL) {
+        head = tail = nuevo;
+        nuevo->sig = NULL;
+        nuevo->ant = NULL;
+    } else {
+        tail->sig = nuevo;
+        nuevo->ant = tail;
+        nuevo->sig = NULL;
+        tail = nuevo;
+    }
+    contador++;
+}
+
+void mostrarAdelante() {
+    if (head == NULL) {
+        cout << "Lista vacia" << endl;
+        return;
+    }
+    Nodo* aux = head;
+    while (aux != NULL) {
+        cout << "[" << aux->id << "] "
+             << aux->nombre << " - "
+             << aux->peso << "kg" << endl;
+        aux = aux->sig;
+    }
+}
+
+void mostrarAtras() {
+    if (tail == NULL) {
+        cout << "Lista vacia" << endl;
+        return;
+    }
+    Nodo* aux = tail;
+    while (aux != NULL) {
+        cout << "[" << aux->id << "] "
+             << aux->nombre << " - "
+             << aux->peso << "kg" << endl;
+        aux = aux->ant;
+    }
+}
+
+void mostrarCantidad() {
+    cout << "Total de paquetes: " << contador << endl;
+}
+
+int main() {
+    int op, id;
+    string nombre;
+    float peso;
+
+    do {
+        cout << "-- MENU ---" << endl;
+        cout << "1. Insertar inicio" << endl;
+        cout << "2. Insertar final" << endl;
+        cout << "3. Mostrar adelante" << endl;
+        cout << "4. Mostrar atras" << endl;
+        cout << "5. Cantidad" << endl;
+        cout << "0. Salir" << endl;
+        cout << "Opcion: ";
+        cin >> op;
+
+        switch (op) {
+            case 1:
+                cin >> id >> nombre >> peso;
+                insertarInicio(id, nombre, peso);
+                break;
+            case 2:
+                cin >> id >> nombre >> peso;
+                insertarFinal(id, nombre, peso);
+                break;
+            case 3:
+                mostrarAdelante();
+                break;
+            case 4:
+                mostrarAtras();
+                break;
+            case 5:
+                mostrarCantidad();
+                break;
+        }
+    } while (op != 0);
+
+    return 0;
+}
